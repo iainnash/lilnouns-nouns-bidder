8db9337d1844aed7382ba785fe4a921bcaaa851d
Iain <iain@zora.co>
2022-07-19 10:40:35 -0400

    updates

--- src/NounsBidder.sol
+++ src/NounsBidder.sol
@@ -1,17 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
+import {Owned} from "solmate/auth/Owned.sol";
+
 interface INounsAuctionPartial {
   function createBid(uint256 nounId) external payable;
+  function auction() external returns (INounsAuctionHouse.Auction);
 }
 
-contract NounsBidder is Ownable {
+contract NounsBidder is Owned {
   event FundsAdded(address sender, uint256 amount);
 
   INounsAuction public immutable nounsAuction;
   uint256 public maxAuctionBidAmount;
 
-  constructor(INounsAuction _nounsAuction, uint256 maxAuctionBidAmount) {
+  constructor(INounsAuction _nounsAuction, uint256 maxAuctionBidAmount, address owner) Owned(owner) {
     nounsAuction = _nounsAuction;
     maxAuctionBidAmount = _maxAuctionBidAmount;
   }
@@ -21,7 +24,6 @@ contract NounsBidder is Ownable {
   }  
 
   function bid() external {
-    uint256 nounsAuction.
     nounsAuction.bid()
   }
 }
